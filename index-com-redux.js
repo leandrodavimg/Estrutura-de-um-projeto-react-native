a7dc87a9db97fc9fdd433282a97cf64fc65d1b7a
Leandro Freitas <leandrodavimg@gmail.com>
2018-08-31 10:15:40 -0300

    index src

--- index-com-redux.js
+++ index-com-redux.js
@@ -1,12 +1,12 @@
 import './config/ReactotronConfig';
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import createNavigator from 'routes';
 import store from 'store';
 
-export default class App extends Component {
-  render() {
-    const Routes = createNavigator();
-    return (<Provider store={store}><Routes /></Provider>);
-  }
-}
+const App = () => {
+  const Routes = createNavigator();
+  return (<Provider store={store}><Routes /></Provider>);
+};
+
+export default App;
